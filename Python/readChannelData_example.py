070bdc55c9495e1d92b983ca0c51df86863c666d
pcsiszar <55701101+cspiet@users.noreply.github.com>
2023-01-30 13:02:34 +0100

    Update readChannelData_example.py

--- Python/readChannelData_example.py
+++ Python/readChannelData_example.py
@@ -1,3 +1,29 @@
+# Copyright ©2021. Femtonics Ltd. (Femtonics). All Rights Reserved. 
+# Permission to use, copy, modify this software and its documentation for educational,
+# research, and not-for-profit purposes, without fee and without a signed licensing agreement, is 
+# hereby granted, provided that the above copyright notice, this paragraph and the following two 
+# paragraphs appear in all copies, modifications, and distributions. Contact info@femtonics.eu
+# for commercial licensing opportunities.
+# 
+# IN NO EVENT SHALL FEMTONICS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, 
+# INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING OUT OF 
+# THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF FEMTONICS HAS BEEN 
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# 
+# FEMTONICS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+# PURPOSE. THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED 
+# HEREUNDER IS PROVIDED "AS IS". FEMTONICS HAS NO OBLIGATION TO PROVIDE 
+# MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+"""
+Example for reading channel data measurements into numpy array
+Can only read data from one channel
+Checks measurement type and chooses dimensions accordingly
+Reads all frames 
+Compatible with API 1.0 and measurement files created by MESc 4.0
+"""
+
 import sys, os, time, numpy
 import APIFunctions
 import miscFunctions
